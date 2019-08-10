1758f9c8e63ec3d57651d9709a9022e54755b527
sdojjy <sdojjy@qq.com>
2019-08-10 22:58:47 +0800

    update max connection size

--- src/main/java/com/pingcap/tidb/workload/Main.java
+++ src/main/java/com/pingcap/tidb/workload/Main.java
@@ -3,7 +3,7 @@ package com.pingcap.tidb.workload;
 
 public class Main {
 
-    //java -cp tidb-workload-all.jar com.pingcap.tidb.workload.Main 2234 insert 100 4000
+    //java -cp tidb-workload-all.jar com.pingcap.tidb.workload.Main 416790 100
     public static void main(String[] args) throws Exception{
         DbUtil.getInstance().initConnectionPool("jdbc:mysql://aa7e48fbcb9a811e9bc3e0e05a91079b-ed3b031e6d68faca.elb.ap-northeast-1.amazonaws.com:4000/test?useunicode=true&characterEncoding=utf8", "root", "");
         int workId=Integer.parseInt(args[0]);
